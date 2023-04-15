26b7d35a6c137dff19796b84a55de39d3c73b71c
CassandraDurr <durrcass@gmail.com>
2023-04-15 13:38:15 +0200

    Refactor functions and add new functions for A/B testing and the model including genre info

--- explicit_ratings/functions.py
+++ explicit_ratings/functions.py
@@ -112,7 +112,91 @@ def reg_logll(
                 )
             )
     # Now update loglikelihood
-    log_lik = -(lmd / 2) * term - log_lik
+    log_lik = log_lik - (lmd / 2) * term
+    return log_lik
+
+
+def reg_logll_with_genre(
+    u_mat: np.array,
+    v_mat: np.array,
+    f_mat: np.array,
+    tau: float,
+    alpha: float,
+    beta: float,
+    lmd: float,
+    bias_user: np.array,
+    bias_movie: np.array,
+    num_users: int,
+    user_ratings: np.array,
+    user_start_index: list,
+    user_end_index: list,
+    movie_dict: dict,
+) -> float:
+    """Return the regularised log likelihood, with genre information.
+
+    Args:
+        u_mat (np.array): User matrix, U.
+        v_mat (np.array): Movie matrix, V.
+        f_mat (np.array): Features matrix, F.
+        tau (float): Trait vectors regulariser.
+        alpha (float): Bias regulariser.
+        beta (float): Feature vector regulariser.
+        lmd (float): Regulariser.
+        bias_user (np.array): Bias vector for users.
+        bias_movie (np.array): Bias vector for movies.
+        num_users (int): Number of users.
+        user_ratings (np.array): Array of user ids, movie ids and ratings.
+        user_start_index (list): Start index for users.
+        user_end_index (list): End index for users.
+        movie_dict (dict): Dictionary with movieId_order as key and genre info as items.
+
+    Returns:
+        float: Regularised log likelihood, including movie genre information.
+    """
+    log_lik = -(alpha / 2) * np.matmul(bias_user.T, bias_user) - (
+        alpha / 2
+    ) * np.matmul(bias_movie.T, bias_movie)
+    # U, Ut
+    val = 0
+    for row in u_mat:
+        val += np.dot(row, row)
+    log_lik = log_lik - (tau / 2) * val
+    # F, Ft
+    val = 0
+    for row in f_mat:
+        val += np.dot(row, row)
+    log_lik = log_lik - (beta / 2) * val
+    # term with m and n element of omega(m)
+    term = 0
+    for user in range(num_users):
+        # User subset of user ratings
+        user_ratings_subset = user_ratings[
+            user_start_index[user] : user_end_index[user]
+        ]
+        for row in user_ratings_subset:
+            n_movie = row[1]
+            rmn = row[2]
+            term += np.square(
+                rmn
+                - (
+                    np.dot(u_mat[user, :], v_mat[n_movie, :])
+                    + bias_movie[n_movie]
+                    + bias_user[user]
+                )
+            )
+    # Now update loglikelihood
+    log_lik = log_lik - (lmd / 2) * term
+    # Term with features and movie trait vectors
+    val = 0
+    for key in movie_dict:
+        # For each movie
+        sum_feature_vectors = f_mat[movie_dict[key]["genre_values"], :].sum(axis=0)
+        vector = v_mat[key, :] - sum_feature_vectors / np.sqrt(
+            movie_dict[key]["genre_count"]
+        )
+        val += np.dot(vector, vector)
+    log_lik = log_lik - (tau / 2) * val
+
     return log_lik
 
 
@@ -263,9 +347,9 @@ def update_user(
                 v_matrix[n_mov, :].reshape((1, latent_dim)),
             )
         # Cholesky decomposition
-        c, low = cho_factor(lmd * vv_mat + tau_identity_mat)
+        chol_factor, low = cho_factor(lmd * vv_mat + tau_identity_mat)
         user_trait_vector = cho_solve(
-            (c, low), lmd * ratings_term.reshape((latent_dim, 1))
+            (chol_factor, low), lmd * ratings_term.reshape((latent_dim, 1))
         ).reshape(copied_u[user, :].shape)
         # Perform update
         copied_u[user, :] = user_trait_vector
@@ -345,11 +429,190 @@ def update_movie(
                 u_matrix[m_user, :].reshape((1, latent_dim)),
             )
         # Cholesky decomposition
-        c, low = cho_factor(lmd * uu_mat + tau_identity_mat)
+        chol_factor, low = cho_factor(lmd * uu_mat + tau_identity_mat)
         movie_trait_vector = cho_solve(
-            (c, low), lmd * ratings_term.reshape((latent_dim, 1))
+            (chol_factor, low), lmd * ratings_term.reshape((latent_dim, 1))
         ).reshape(copied_v[movie, :].shape)
         # Perform update
         copied_v[movie, :] = movie_trait_vector
 
     return copied_v, copied_b_n
+
+
+def map_genres(genre_list: list[str], genres_dict: dict[str, int]) -> list[str]:
+    """A function that can be used to map genre names as strings to indices for genres.
+
+    Args:
+        genre_list (list[str]): A list of genre names to be converted to integers (indices).
+        genres_dict (dict[str, int]): Dictionary with genre names as keys and genre indices as items.
+
+    Returns:
+        list[str]: A list of genre indices converted from strings using a mapping.
+    """
+    return [genres_dict[genre] for genre in genre_list]
+
+
+def genre_key_dict(file_location: str, genres: dict[str, int]) -> dict[int, list[int]]:
+    """Produce a dictionary with genre id as key and movie ids as items.
+
+    Args:
+        file_location (str): Location of genres csv.
+        genres (dict[str, int]): A dictionary relating genre names to genre ids.
+
+    Returns:
+        dict[int, list[int]]: Dictionary with genre as key and movies as items.
+
+    """
+    movie_genres = pd.read_csv(file_location, converters={"genres_v2": pd.eval})
+    # Drop irrelevant columns and rename 'genres_v2'
+    movie_genres.drop(
+        columns=["Unnamed: 0", "genres", "movieId", "title"], inplace=True
+    )
+    movie_genres.rename(columns={"genres_v2": "genre_names"}, inplace=True)
+    movie_genres["genre_values"] = movie_genres["genre_names"].apply(
+        lambda row: map_genres(row, genres)
+    )
+    movie_genres["genre_count"] = movie_genres["genre_values"].apply(len)
+    movie_genres.drop(columns=["genre_names"], inplace=True)
+    # Create genre dictionary
+    genre_dict = {}
+    for index, row in movie_genres.iterrows():
+        # for each genre in a movies list of genres
+        for genre_value in row["genre_values"]:
+            # if the key already exists
+            if genre_value in genre_dict:
+                genre_dict[genre_value].append(row["movieId_order"])
+            # if the key does not exist
+            else:
+                genre_dict[genre_value] = [row["movieId_order"]]
+    return genre_dict
+
+
+def simulate_user(
+    preferred_genres: list[int], genre_dict: dict[int, list[int]]
+) -> tuple[list[int], list[float], str]:
+    """Function used in A/B testing to simulate user history and grouping.
+
+    Args:
+        preferred_genres (list[int]): Genre ids that a user likes.
+        genre_dict (dict[int, list[int]]): A dictionary where a genre is a key and the movie ids with that tag are items.
+
+    Returns:
+        tuple[list[int], list[float], str]: Movie ids a user enjoyed, their corresponding ratings and the user's group.
+    """
+    # Randomly sample U(10, 20) movies from the genres
+    movie_ids_history = []
+    for genre in preferred_genres:
+        movie_ids_history.append(
+            np.random.choice(
+                genre_dict[genre], size=np.random.randint(low=5, high=11), replace=False
+            ).tolist()
+        )
+    movie_ids_history = [movie for sublist in movie_ids_history for movie in sublist]
+    # Randomly sample high ratings for said movies
+    movie_ratings = np.random.choice(
+        [8.0, 8.5, 9.0, 9.5, 10.0], size=len(movie_ids_history)
+    ).tolist()
+    # Randomly assign the user to a group
+    group = np.random.choice(["A", "B"])
+
+    return movie_ids_history, movie_ratings, group
+
+
+def find_user_trait_vector(
+    u_mat: np.array,
+    v_mat: np.array,
+    bias_movie: np.array,
+    lmd: float,
+    tau: float,
+    usr_history: list[int],
+    usr_ratings: list[float],
+) -> np.array:
+    """Find the user trait vector for a new user with a set of previous ratings.
+
+    Assume a user bias of zero.
+
+    Args:
+        u_mat (np.array): Trained user matrix, U.
+        v_mat (np.array): Trained movie matrix, V.
+        bias_movie (np.array): Trained movie biases, b_n^i.
+        lmd (float): Objective function regulariser, lambda.
+        tau (float): User matrix regulariser, tau.
+        usr_history (list[int]): List of movie ids that a user rated.
+        usr_ratings (list[float]): User's ratings of movies.
+
+    Returns:
+        np.array: User trait vector.
+    """
+    # Derive latent dimension
+    latent_dim = u_mat.shape[1]
+    # Compute user trait vector
+    # Assume zero user bias
+    ratings_term = 0
+    vv_mat = 0
+    # Iterate over movies rated by user
+    for cnt, movie_id in enumerate(usr_history):
+        ratings_term += (usr_ratings[cnt] - bias_movie[movie_id] - 0) * v_mat[
+            movie_id, :
+        ]
+        vv_mat += np.matmul(
+            v_mat[movie_id, :].reshape((latent_dim, 1)),
+            v_mat[movie_id, :].reshape((1, latent_dim)),
+        )
+    # Use cholesky decomposition to find the user trait vector
+    chol_factor, low = cho_factor(lmd * vv_mat + tau * np.identity(latent_dim))
+    user_trait_vector = cho_solve(
+        (chol_factor, low), lmd * ratings_term.reshape((latent_dim, 1))
+    ).reshape((latent_dim,))
+    return user_trait_vector
+
+
+def top_n_recommendations(
+    user_trait_vector: np.array,
+    v_mat: np.array,
+    bias_movie: np.array,
+    movie_ids: pd.DataFrame,
+    num_recom: int,
+    remove_movies_limit: int,
+    ratings: pd.DataFrame,
+) -> pd.DataFrame:
+    """Generate top n recommendations given a user trait vector.
+
+    Args:
+        user_trait_vector (np.array): Encoding of user preference.
+        v_mat (np.array): Trained movie matrix, V.
+        bias_movie (np.array): Trained movie biases, b_n^i.
+        movie_ids (pd.DataFrame): Dataframe relating movie_ids and adapted ids.
+        num_recom (int): Number of recommendations to return.
+        remove_movies_limit (int): Exclude movies that fewer than remove_movies_limit users rated.
+        ratings (pd.DataFrame): Dataframe of user-movie pairs with ratings.
+
+    Returns:
+        pd.DataFrame: Movie ids and corresponding scores.
+    """
+    number_of_movies = v_mat.shape[0]
+    # Compute score for each movie
+    score_for_movie = np.zeros((number_of_movies,))
+    for movie in range(number_of_movies):
+        score_for_movie[movie] = (
+            np.dot(user_trait_vector, v_mat[movie, :]) + 0.05 * bias_movie[movie]
+        )
+    movie_scores = pd.DataFrame()
+    movie_scores["Scores"] = score_for_movie
+    movie_scores = movie_scores.reset_index()
+    movie_scores.columns = ["movieId_order", "Scores"]
+    movie_scores = pd.merge(movie_scores, movie_ids)
+    movie_scores = movie_scores.sort_values(by="Scores", ascending=False)
+    # Remove movies that were only rated by a few people
+    movie_id_exclude = list(
+        ratings["movieId_order"]
+        .value_counts()
+        .loc[lambda x: x < remove_movies_limit]
+        .to_frame()
+        .index
+    )
+    movie_scores_exclude = movie_scores[
+        ~movie_scores["movieId_order"].isin(movie_id_exclude)
+    ]
+
+    return movie_scores_exclude.head(num_recom)
